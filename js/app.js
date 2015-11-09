242470abe02281a4806ad9af813257c554c68e43
William Menten-Weil <wtmenten@gmail.com>
2015-11-09 08:57:26 -0800

    removed html5 mode

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ var app = angular.module('processBook', ['ngRoute'])
   })
 
   // configure html5 to get links working on jsfiddle
-  // $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(true);
 
   // angular.module('processBook').run(function($rootScope, $templateCache) {
   //      $rootScope.$on('$viewContentLoaded', function() {
